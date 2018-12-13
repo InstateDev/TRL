5e8dd2b33f8d6240085c0816f7ccf03ec2909728
Joao Santos <jrmsantos15@gmail.com>
2018-12-13 18:02:50 +0000

    fix(TRL): Updates migrations to new role registries

--- migrations/5_reg_factory.js
+++
@@ -1,8 +0,0 @@
-const config = require('../config')
-const OwnedRegistryFactoryContract = artifacts.require('@frontier-token-research/role-registries/contracts/OwnedRegistryFactory')
-
-module.exports = (deployer) => {
-  if (config.proxyMigration) {
-    deployer.deploy(OwnedRegistryFactoryContract)
-  }
-}
--- migrations/5_registries.js
+++ migrations/5_registries.js
@@ -3,6 +3,8 @@ const config = require('../config')
 const Standard20TokenContract = artifacts.require('Standard20TokenMock')
 const keccak256 = require('js-sha3').keccak256
 const OwnedRegistryFactoryContract = artifacts.require('@frontier-token-research/role-registries/contracts/OwnedRegistryFactory')
+const OwnedRegistryContract = artifacts.require('@frontier-token-research/role-registries/contracts/OwnedRegistry')
+const PeriodContract = artifacts.require('Daily')
 
 module.exports = (deployer) => {
   deployer.then(async () => {
@@ -10,6 +12,17 @@ module.exports = (deployer) => {
       const RegistryFactory = await OwnedRegistryFactoryContract.deployed()
       await RegistryFactory.newRegistry(keccak256('voter'))
       await RegistryFactory.newRegistry(keccak256('candidate'))
+      const candidateAddress = await RegistryFactory.getRegistry(keccak256('candidate'))
+      const voterAddress = await RegistryFactory.getRegistry(keccak256('voter'))
+
+      let periodAddress = await PeriodContract.deployed()
+      periodAddress = periodAddress.address
+
+      const candidateInstance = OwnedRegistryContract.at(candidateAddress)
+      const voterInstance = OwnedRegistryContract.at(voterAddress)
+
+      await candidateInstance.init(5, 5, periodAddress)
+	  await voterInstance.init(5, 5, periodAddress)
     }
   })
 }
